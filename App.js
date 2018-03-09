0be97e5fcdb0bd2261f21bbbb485ec5a7376a7ce
Emmanuel Corrales <emc@Emmanuels-MacBook-Air.local>
2018-03-10 03:46:39 +0800

    Show scanner after clicking the scan button.

--- App.js
+++ App.js
@@ -2,19 +2,27 @@ import React from 'react';
 import { Button, StyleSheet, View } from 'react-native';
 
 import History from './components/history'
+import Scanner from './components/scanner'
 
 export default class App extends React.Component {
-  scan = () => {};
+  state = {
+    scanning: false
+  }
+  scan = () => {
+    this.setState({ scanning: true });
+  };
 
   render() {
     return (
-      <View style={ styles.container }>
-        <History />
-        <Button
-          style={ styles.button }
-          title={ "SCAN" }
-          onPress={ this.scan } />
-      </View>
+      this.state.scanning ?
+        <Scanner /> :
+        <View style={ styles.container }>
+          <History />
+          <Button
+            style={ styles.button }
+            title={ "SCAN" }
+            onPress={ this.scan } />
+        </View>
     );
   }
 }
